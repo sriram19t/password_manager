de5ca48cf070b55ebf2856b7e9e947442fd234ea
sriram19t <saisriramthota@gmail.com>
2024-09-26 01:13:50 +0530

    changes

--- create_key.py
+++ create_key.py
@@ -1,1 +1,7 @@
+from cryptography.fernet import Fernet
 
+def write_key():
+    Key=Fernet.generate_key()
+    with open("key.key","wb") as file:
+        file.write(Key)
+write_key()
--- pmg.py
+++ pmg.py
@@ -1,1 +1,49 @@
+from cryptography.fernet import Fernet
+import pandas as pd
+
+
+def load_key():
+    file=open('key.key',"rb")
+    return file.read()
+
+
+key=load_key()
+fer=Fernet(key)
+
+def add():
+    user=input("Enter your Account name:")
+    password=input("Enter Your password")
+    with open("password.txt",'a') as f:
+        f.write(user+"->"+fer.encrypt(password.encode()).decode()+"\n")
+    print("Successfully added!")
+
+def view():
+    try:
+        with open("password.txt",'r') as f:
+            print("format is in user:password mode")
+            print({i.rsplit()[0].split('->')[0]:fer.decrypt(i.rsplit()[0].split('->')[1]).decode() for i in f.readlines()})
+    except Exception as e:
+        print(f"An error occurred: {e}")
+while True:
+    manage=input("Do you wanna manage your passwords (Y/N)")
+
+
+    if manage.upper()=='Y':
+        action=input("Do you wanna add or view password (ADD/VIEW)")
+
+        if action.upper()=='ADD':
+            add()
+            pass
+        if action.upper()=='VIEW':
+            view()
+            pass
+    else:
+        quit()
+
+
+
+
+
+
+
 
